d7af40d36112ad008a027304e736aea369dc75d6
roboXcode1 <ovichowdhuryflixy@gmail.com>
2025-07-25 17:26:39 +0600

    1st problen done

--- problem-1.js
+++ problem-1.js
@@ -1,6 +1,14 @@
-function name(str){
+function calculateVAT( price ) {
+    if(typeof price   !== 'number' || price < 0 ){
+        return ' "Invalid" '
+    }
+    else{
+    let vat = price * 7.5 / 100;
+    return vat;
+    }
 
-    console.log(nm)
 }
-  nm= 'ovi';
- name(nm)
+
+  console .log(calculateVAT(-434))
+  console .log(calculateVAT('rgr'))
+   console .log(calculateVAT(54))
